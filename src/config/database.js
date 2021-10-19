899551760e65786942578b1e9e79e772bd148815
Arthur <arthur.04.pb@gmail.com>
2021-10-19 20:13:29 -0300

    Adicionando e configurando sequelize

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+  host: "localhost",
+  username: "docker",
+  password: "docker",
+  database: "forca",
+  dialect: "postgres",
+  operatorsAliases: false,
+  logging: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
