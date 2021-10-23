a0b4236df90770493a37e13c3b5e6e87ff5d7ff1
Arthur Camurça <arthur.04.pb@gmail.com>
2021-10-22 21:29:10 -0300

    criando rota para checar letras

--- src/controllers/game.controller.js
+++ src/controllers/game.controller.js
@@ -4,7 +4,8 @@ const { Game } = require("../app/models");
 module.exports = {
   registerNewWord: async (req, res) => {
     const { theme, word } = req.body;
-    const response = await Game.create({ theme, word });
+    const wordLowCase = word.toLowerCase();
+    const response = await Game.create({ theme, word: wordLowCase });
     return res.status(201).json(response);
   },
 
@@ -13,7 +14,23 @@ module.exports = {
       order: Sequelize.literal("random()"),
     });
 
-    const { id, word } = response;
-    return res.status(200).json({ id, word });
+    const { id, theme } = response;
+    return res.status(200).json({ id, theme });
+  },
+
+  checkLetter: async (req, res) => {
+    const { id, letter } = req.body;
+    const game = await Game.findByPk(id);
+    if (game) {
+      const { word } = game;
+      const indexOfLetter = word.indexOf(letter.toLowerCase());
+
+      if (indexOfLetter !== -1) {
+        return res.status(200).json(indexOfLetter);
+      }
+      return res.status(404).json({ message: "Letter not found" });
+    } else {
+      return res.status(404).json({ message: "Game not found" });
+    }
   },
 };
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const routes = express.Router();
 const gameController = require("./controllers/game.controller");
 
 routes.post("/create/word", gameController.registerNewWord);
+routes.post("/check/letter", gameController.checkLetter);
 routes.get("/start/game", gameController.startGame);
 
 module.exports = routes;
